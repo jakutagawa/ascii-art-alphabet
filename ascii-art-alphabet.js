c2698b14fe4a2cdafd18ee721ff0a45601ea9497
Jon Akutagawa <20073293+jakutagawa@users.noreply.github.com>
2018-10-21 23:18:43 -0700

    script working

--- ascii-art-alphabet.js
+++ ascii-art-alphabet.js
@@ -1,17 +1,71 @@
-console.log('Hello, world')
+#! /usr/local/bin/node
 
-var x = +process.argv[2];
-var y = +process.argv[3];
-console.log((x + y) / 2);
+var wordInput = process.argv[2];
+console.log('You entered ' + wordInput);
+var lowerCapWordInput = wordInput.toLowerCase();
 
+var lineNum = 0
+var lineArray = [];
+var asciiArray = [];
+var colNum = 0;
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-var z = process.argv[2];
-var w = process.argv[3];
-console.log(z + w);
-console.log(z);
-console.log(w);
-console.log((z+w)/2);
+var fs = require('fs');
+var readline = require('readline');
 
+convertWord(callback => {
+    array = callback
 
-var s = 'String\n'
-console.log(s[0])
+    var rowString = ''
+    var rowArray = ['', '', '', '', '', '', '', '', '', '', '', '']
+    for (var j = 0; j < lowerCapWordInput.length; j++) {
+        if (!alphabet.includes(lowerCapWordInput[j])) {
+            console.log('Input included invalid characters: ' + lowerCapWordInput[j]);
+            break
+            //throw new Error ("wrong characters")
+        } else {
+            var charCode = lowerCapWordInput[j].charCodeAt(0) - 32;
+            for (var row = 0; row < 12; row++) {
+                rowArray[row] += array[charCode][row]
+            }
+        }
+    }
+    for (var row = 0; row < 12; row++) {
+        console.log(rowArray[row])
+    }
+})
+
+function convertWord(callback) {
+    var fontfile = readline.createInterface({
+        input: fs.createReadStream('poison.flf'),
+        terminal: false
+    })
+    fontfile.on('line', function(line) {
+        lineNum++;
+        var strippedLine = '';
+        if (lineNum > 15) {
+            for (var i = 0; i < line.length - 1; i++) {
+                if ((colNum == 11) && (i == (line.length - 2))) {
+                    strippedLine += ''
+                } else if (line[i] == '$') {
+                    strippedLine += ' '
+                } else {
+                    strippedLine += line[i]
+                }
+            }
+
+            lineArray.push(strippedLine)
+
+            if (colNum == 11) {
+                asciiArray.push(lineArray)
+                lineArray = [];
+                colNum = 0;
+            } else {
+                colNum++;
+            }
+        }
+    })
+    fontfile.on('close', function() {
+        callback(asciiArray)
+    });
+}

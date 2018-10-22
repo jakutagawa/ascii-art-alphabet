23262b6d677570a8a8eb331dfc2454324263e44d
Jon Akutagawa <20073293+jakutagawa@users.noreply.github.com>
2018-10-21 19:37:52 -0700

    arguments

--- hello.js
+++ hello.js
@@ -1,1 +1,17 @@
 console.log('Hello, world')
+
+var x = +process.argv[2];
+var y = +process.argv[3];
+console.log((x + y) / 2);
+
+
+var z = process.argv[2];
+var w = process.argv[3];
+console.log(z + w);
+console.log(z);
+console.log(w);
+console.log((z+w)/2);
+
+
+var s = 'String\n'
+console.log(s[0])
